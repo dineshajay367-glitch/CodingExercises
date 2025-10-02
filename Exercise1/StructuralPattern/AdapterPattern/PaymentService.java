e813820861caa49ac6baae5d61eda9aef69d076e
dineshajay367-glitch <dineshajay367@gmail.com>
2025-10-02 22:07:29 +0530

    initial commit

--- Exercise1/StructuralPattern/AdapterPattern/PaymentService.java
+++ Exercise1/StructuralPattern/AdapterPattern/PaymentService.java
@@ -1,8 +1,11 @@
 package AdapterPattern;
 
+import java.util.logging.Logger;
+
 public class PaymentService {
+    private static final Logger logger= Logger.getLogger(PaymentService.class.getName());
     public void paymentProcess(Currency currency)
     {
-        System.out.println("Payment Processed: "+ currency.getAmount());
+        logger.info("Payment Processed: "+ currency.getAmount());
     }
 }
