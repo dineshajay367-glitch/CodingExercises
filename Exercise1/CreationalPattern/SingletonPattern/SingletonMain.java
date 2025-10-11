92250c97efc050c65a11461e1bf9d18845cee91b
dineshajay367-glitch <dineshajay367@gmail.com>
2025-10-11 17:45:11 +0530

    cf

--- Exercise1/CreationalPattern/SingletonPattern/SingletonMain.java
+++ Exercise1/CreationalPattern/SingletonPattern/SingletonMain.java
@@ -3,9 +3,11 @@ package SingletonPattern;
 public class SingletonMain {
     public static void main(String[] args)
     {
-        Runnable task = () ->{
-            Singleton s1=Singleton.getInstance();
-            System.out.println(Thread.currentThread().getName()+" "+s1.hashCode());
+        Runnable task = new Runnable() {
+            public void run() {
+                Singleton s1 = Singleton.getInstance();
+                System.out.println(Thread.currentThread().getName() + " " + s1.hashCode());
+            }
         };
         Thread t1=new Thread(task,"thread1");
         Thread t2=new Thread(task,"thread2");
